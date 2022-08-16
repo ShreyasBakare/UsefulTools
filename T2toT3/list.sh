2e19232a103f3eed76bac438f0cea663935994ee
Prachurjya Hazarika <prachurjyahazarika@gmail.com>
2022-08-16 10:09:24 +0530

    Updated file

--- T2toT3/list.sh
+++ T2toT3/list.sh
@@ -1,9 +1,10 @@
 #!/bin/sh                                                                                                                                
 for basedir in /store/user/arastogi/SingleElectron/SingleElectronRun2016Ev1_Nano25Oct2019_RHN_20200117_080050/200117_070140
 do
-    (xrd se01.indiacms.res.in dirlistrec /cms/$basedir) > list4.txt
-    awk '{print $5 > "list3.txt" }' \list4.txt
-    awk '{print substr($1, 6); }' \list3.txt > list2.txt
-    cat list2.txt |grep root > list1.txt
+    (xrdfs se01.indiacms.res.in ls /cms/$basedir) > list1.txt
+    #(xrd se01.indiacms.res.in dirlistrec /cms/$basedir) > list4.txt
+    #awk '{print $5 > "list3.txt" }' \list4.txt
+    #awk '{print substr($1, 6); }' \list3.txt > list2.txt
+    #cat list2.txt |grep root > list1.txt
 done;
 
